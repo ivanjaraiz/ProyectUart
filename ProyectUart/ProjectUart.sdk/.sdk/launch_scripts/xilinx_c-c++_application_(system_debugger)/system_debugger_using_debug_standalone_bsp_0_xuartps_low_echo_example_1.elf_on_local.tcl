2598df0fc019447ded32295dd2f070d72021c852
ijaraiz <ijaraiz@sainsel.es>
2019-01-14 17:40:07 +0100

    Este ejemplo necesita una tarjeta externa

---
+++ ProyectUart/ProjectUart.sdk/.sdk/launch_scripts/xilinx_c-c++_application_(system_debugger)/system_debugger_using_debug_standalone_bsp_0_xuartps_low_echo_example_1.elf_on_local.tcl
@@ -0,0 +1,13 @@
+connect -url tcp:127.0.0.1:3121
+source C:/Xilinx/Xilinx_Training/ProyectUart/ProjectUart.sdk/designbd_uart_wrapper_hw_platform_0/ps7_init.tcl
+targets -set -nocase -filter {name =~"APU*" && jtag_cable_name =~ "Digilent Zybo Z7 210351A78402A"} -index 0
+loadhw C:/Xilinx/Xilinx_Training/ProyectUart/ProjectUart.sdk/designbd_uart_wrapper_hw_platform_0/system.hdf
+targets -set -nocase -filter {name =~"APU*" && jtag_cable_name =~ "Digilent Zybo Z7 210351A78402A"} -index 0
+stop
+ps7_init
+ps7_post_config
+targets -set -nocase -filter {name =~ "ARM*#0" && jtag_cable_name =~ "Digilent Zybo Z7 210351A78402A"} -index 0
+rst -processor
+targets -set -nocase -filter {name =~ "ARM*#0" && jtag_cable_name =~ "Digilent Zybo Z7 210351A78402A"} -index 0
+dow C:/Xilinx/Xilinx_Training/ProyectUart/ProjectUart.sdk/standalone_bsp_0_xuartps_low_echo_example_1/Debug/standalone_bsp_0_xuartps_low_echo_example_1.elf
+bpadd -addr &main
